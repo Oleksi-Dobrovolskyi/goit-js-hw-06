d69e6373ab1d80d67acf2d3fab9ca04d1017b866
Oleksi-Dobrovolskyi <alex.puhovski@gmail.com>
2024-12-09 23:25:30 +0100

    Add files via upload

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,19 @@
 class Storage {
+  #items;
   constructor(items) {
-    this.items = items; 
+    this.#items = items; 
   }
   getItems() {
-    return this.items;
+    return this.#items;
   }
 
   addItem(item) {
-    this.items.push(item);
+    this.#items.push(item);
   }
   removeItem(item) {
-    const index = this.items.indexOf(item); 
+    const index = this.#items.indexOf(item); 
     if (index !== -1) {
-      this.items.splice(index, 1);
+      this.#items.splice(index, 1);
     } else {
       console.log(`Item "${item}" not found in storage.`);
     }
